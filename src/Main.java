1c990f84779ba73d7b390c2b8445f610bcbe387d
Rémi DURET <rd.duret@hotmail.fr>
2019-06-14 10:24:24 +0200

    Add files via upload
    epurage suppression des caracteres spéciaux

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@
 public class Main {
 	public static void main(String[] args) {
 		Affichage aff = new Affichage();
-		Partie p = new Partie("", "");
+		Partie p = new Partie();
 
-		
-		/*String path=new File("").getAbsolutePath(); 
-		new File(path+"\\sauvegarde.txt"); 
-		
+
+		/*String path=new File("").getAbsolutePath();
+		new File(path+"\\sauvegarde.txt");
+
 		try{
 		      PrintWriter sortie = new PrintWriter(new BufferedWriter(new FileWriter("sauvegarde.txt")));
 		      sortie.print("a2\na3\n");
@@ -19,20 +19,20 @@ public class Main {
 		      System.out.println(e);
 		    }
 
-		
+
 		p.charger();
 		aff.afficher(p.getPlateau());
-		for(int i=0;i<3;i++) {			
+		for(int i=0;i<3;i++) {
 			p.jouerCoup(p.getJoueur(i%2));
 			aff.afficher(p.getPlateau());
 		}
-		
+
 		String s = p.getBackup();
 		System.out.println(s);
 		p.sauvegarder();
 		System.out.println("chargement");
 		p.charger();
 		System.out.println("deb : " + p.getTour() + p.getJoueur(0).getPseudo() + "1 " + p.getJoueur(1).getPseudo());*/
-		
+
 	}
 }
